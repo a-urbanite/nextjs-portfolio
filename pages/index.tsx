dc7ba1fee27043a2e82e2960cf34988402a1a984
a.urbanite <a.urbanite@gmail.com>
2022-08-01 13:21:23 +0200

    css adjustmens

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/pages/Home.module.css'
 
 import CV from '../components/CV'
 import Collapsable from '../components/Collapsable'
@@ -10,11 +10,11 @@ import { FaGithubSquare, FaLinkedin } from 'react-icons/fa'
 const Home: NextPage = () => {
   return (
     <>
-      <h1 className='title'>Alexander Städtler</h1>
-      <div className='profileBox'>
-        <img className='profilePic' src='/images/profile_pic_light.png' alt='profile pic'></img>
-        <div className='profileBlock'>
-          <p className='profileText'>
+      <h1 className={styles.title}>Alexander Städtler</h1>
+      <div className={styles.profileBox}>
+        <img className={styles.profilePic} src='/images/profile_pic_light.png' alt='profile pic'></img>
+        <div className={styles.profileBlock}>
+          <p className={styles.profileText}>
             Enthusiastic Fullstack Dev from Germany 
             with a pivot towards backend. My background in 
             archaeology has taught me a great deal about managing 
@@ -23,17 +23,15 @@ const Home: NextPage = () => {
             teams, clearly formulated goals and priorities, 
             as well as agile structures and strategies.
           </p>
-          <div className="icon__wrapper">
-            <a className='icon' href='https://github.com/a-urbanite' target='blank'><FaGithubSquare /></a>
-            <a className='icon' href='https://www.linkedin.com/in/alexander-staedtler/' target='blank'><FaLinkedin/></a>
+          <div className={styles.icon__wrapper}>
+            <a className={styles.icon} href='https://github.com/a-urbanite' target='blank'><FaGithubSquare /></a>
+            <a className={styles.icon} href='https://www.linkedin.com/in/alexander-staedtler/' target='blank'><FaLinkedin/></a>
           </div>
         </div>
       </div>
-      <div className='CV_wrapper'>
-        <Collapsable collapsed={true} titleOpen="collapse content" titleClosed="Show whole CV" classname="collapse-button">
-          <CV/>
-        </Collapsable>
-      </div>
+      <Collapsable collapsed={true} titleOpen="collapse content" titleClosed="Show whole CV" classname="collapse-button">
+        <CV/>
+      </Collapsable>
     </>
   )
 }
